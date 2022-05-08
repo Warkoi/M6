eb88bbc139220dedbb000d28c6ccfc6cb6f468c4
Albert <ajcatano@gmail.com>
2022-05-07 23:10:50 -0400

    -- Add Map annotations

--- City Sights App/Views/Home/HomeView.swift
+++ City Sights App/Views/Home/HomeView.swift
@@ -42,6 +42,7 @@ struct HomeView: View {
                     // show map
                     BusinessMap()
                         .ignoresSafeArea()
+                    
                 }
             }
             
--- City Sights App/Views/Home/Map/BusinessMap.swift
+++ City Sights App/Views/Home/Map/BusinessMap.swift
@@ -34,6 +34,8 @@ struct BusinessMap: UIViewRepresentable {
     func makeUIView(context: Context) -> MKMapView {
         
         let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        
         
         // Make the suer show up on the map
         mapView.showsUserLocation = true
@@ -55,5 +57,41 @@ struct BusinessMap: UIViewRepresentable {
     }
     static func dismantleUIView(_ uiView: MKMapView, coordinator: ()) {
         uiView.removeAnnotations(uiView.annotations)
+    }
+    
+    // MARK - Coordinator Class
+    func makeCoordinator() -> Coordinator {
+        return Coordinator()
+    }
+    
+    class Coordinator: NSObject, MKMapViewDelegate {
+        
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            
+            // If the annotation is the user blue dot, return nil
+            if annotation is MKUserLocation {
+                return nil
+            }
+            
+            // Check if there's a reusable annotation view first
+            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: Constants.annotationReuseID)
+            
+            if annotationView == nil {
+                // Create an annotation view
+                annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: Constants.annotationReuseID )
+                
+                annotationView!.canShowCallout = true
+                annotationView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            }
+            else {
+                // We got a reusable one
+                annotationView!.annotation = annotation
+            }
+            
+           
+            
+            //Return it
+            return annotationView
+        }
     }
 }
